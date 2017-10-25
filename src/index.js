62d4c53813acf6c99f802f3b132f26416fb25338
Camilo <camilogo1@yahoo.com>
2017-10-25 16:13:37 -0500

    Added html-webpack-plugin and configured output

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './styles/common.css';
 import SAlogo from './images/SAlogo.svg';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     element.innerHTML = _.join(['Beware', 'Silver', 'Abuse'], '! ');
     element.classList.add('hello');
@@ -12,6 +14,11 @@ function component() {
     SAlogoImg.src = SAlogo;
 
     element.appendChild(SAlogoImg);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
