bfc7b3d9f49c23205cd611fe7efb70c021d5506a
Camilo <camilogo1@yahoo.com>
2017-10-27 14:53:50 -0500

    Divided the prod and dev config files so they export and behave differently.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import _ from 'lodash';
 import './styles/common.css';
+import _ from 'lodash';
+import { cube } from './math.js';
 import SAlogo from './images/SAlogo.svg';
 import printMe from './print.js';
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 
 function component() {
     var element = document.createElement('div');
@@ -24,3 +30,11 @@ function component() {
 }
 
 document.body.appendChild(component());
+console.log('Hello webpack! 5 cubed is equal to ', cube(5));
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
